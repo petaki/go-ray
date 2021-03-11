4bd2cd28872e3515978b6bb231cc68c65d49a9cf
Peter Takacs <peter.takacs@koodilab.com>
2021-03-11 23:27:18 +0100

    Added uuid

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/petaki/go-ray
 
 go 1.16
+
+require github.com/google/uuid v1.2.0 // indirect
--- ray.go
+++ ray.go
@@ -1,13 +1,17 @@
 package ray
 
+import "github.com/google/uuid"
+
 // Ray type.
 type Ray struct {
+	uuid     string
 	settings *Settings
 }
 
 // New function.
 func New(settings *Settings) *Ray {
 	r := new(Ray)
+	r.uuid = uuid.NewString()
 
 	if settings != nil {
 		r.settings = settings
