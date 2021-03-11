9856127994c5168234556e28e68d662d17ba7a9a
Peter Takacs <peter.takacs@koodilab.com>
2021-03-11 01:15:34 +0100

    Enhanced the tests

--- ray_test.go
+++ ray_test.go
@@ -3,18 +3,35 @@ package ray
 import "testing"
 
 func TestNew(t *testing.T) {
-	r := New(nil)
-
-	if r == nil {
-		t.Errorf("expected: not nil, got: nil")
+	tests := []struct {
+		name         string
+		client       *Client
+		expectedHost string
+		expectedPort int
+	}{
+		{"Default Settings", nil, "localhost", 23517},
+		{"Custom Settings", &Client{Host: "192.168.100.1", Port: 23518}, "192.168.100.1", 23518},
 	}
 
-	r = New(&Client{
-		Host: "192.168.100.1",
-		Port: 23517,
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			r := New(tt.client)
 
-	if r == nil {
-		t.Errorf("expected: not nil, got: nil")
+			if r == nil {
+				t.Errorf("expected: not nil, got: nil")
+			} else {
+				if r.client == nil {
+					t.Errorf("expected: not nil, got: nil")
+				} else {
+					if r.client.Host != tt.expectedHost {
+						t.Errorf("expected: %s, got: %s", tt.expectedHost, r.client.Host)
+					}
+
+					if r.client.Port != tt.expectedPort {
+						t.Errorf("expected: %d, got: %d", tt.expectedPort, r.client.Port)
+					}
+				}
+			}
+		})
 	}
 }
